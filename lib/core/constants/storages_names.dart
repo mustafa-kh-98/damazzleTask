b2aff76d1b72f7dbff6c458e6a5e8044e2169a69
mustafa-kh <mustafasyr366@gmail.com>
2025-02-15 16:10:01 +0300

    fix bug

--- lib/core/constants/storages_names.dart
+++ lib/core/constants/storages_names.dart
@@ -1,6 +1,4 @@
 abstract class StoragesNames {
   static const String app = 'app';
   static const String darkMode = 'darkMode';
-  static const String appCountries = 'appCountries';
-  static const String appUrls = 'appUrls';
 }
